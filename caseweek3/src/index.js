5a5f65ba3eb421681fc319ec137f56ab9752ce85
emkax <michaelkristianto15@gmail.com>
2026-03-22 19:04:37 +0700

    add : rate limiter

--- caseweek3/src/index.js
+++ caseweek3/src/index.js
@@ -5,11 +5,21 @@ import userRoute from "./routes/user.route.js"
 
 import { errorHandler } from "./middleware/errorHandler.js";
 
+import rateLimit from "express-rate-limit";
+
+const limiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 5,
+    message: {message : "too many login attempts, try again later"}
+})
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.use(limiter);
 
 app.use("/api/news",newsRoute);
 app.use("/api/user",userRoute);
